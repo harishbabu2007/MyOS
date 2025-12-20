a7684435effb6f53a9285c807ef1fa73944e2346
Viral Parikh <parikhviral02@gmail.com>
2025-12-20 17:30:58 +0530

    changes done

--- src/sutils/sutils.h
+++ src/sutils/sutils.h
@@ -5,9 +5,12 @@
 #include <stddef.h>
 #include <stdint.h>
 
-size_t strlen(const char* str);
-void reverse_str(char* str, int length);
-char* itoa(int num, char* str, int base);
+size_t strlen(const char *str);
+void reverse_str(char *str, int length);
+char *itoa(int num, char *str, int base);
 void *memset(void *dest, int val, size_t len);
+char *strcpy(char *dest, const char *src);
+int strcmp(const char *str1, const char *str2);
+char *strconcat(char *dest, const char *src);
 
 #endif
